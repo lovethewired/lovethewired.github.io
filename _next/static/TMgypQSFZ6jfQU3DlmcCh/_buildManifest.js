04990f4d6c05bea75a36be7fe971ec4a6dad28f6
willisk <kurtwillis@live.com>
2022-03-07 13:16:16 +0100

    gh-pages deploy

--- _next/static/TMgypQSFZ6jfQU3DlmcCh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-e4603a7f8139a636.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/blog/[slug]":["static/chunks/pages/blog/[slug]-634e68f9ffe7d75b.js"],sortedPages:["/","/_app","/_error","/blog/[slug]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
